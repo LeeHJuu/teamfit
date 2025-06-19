2a3cd35eb5217e65ea2df46795c59ded34c2c7a5
LeeHJuu <xyident903@gmail.com>
2025-06-19 13:50:57 +0900

    feat: 디자인시스템 스타일 추가

---
+++ lib/src/config/theme/custom_color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static Color gray_10 = Color(0x1a1a1a);
+  static Color gray_20 = Color(0x333333);
+  static Color gray_30 = Color(0x4d4d4d);
+  static Color gray_40 = Color(0x666666);
+  static Color gray_50 = Color(0x808080);
+  static Color gray_60 = Color(0x999999);
+  static Color gray_70 = Color(0xb3b3b3);
+  static Color gray_80 = Color(0xcccccc);
+  static Color gray_90 = Color(0xe6e6e6);
+  static Color gray_95 = Color(0xf2f2f2);
+
+  static Color primary_05 = Color(0x190500);
+  static Color primary_10 = Color(0x330900);
+  static Color primary_20 = Color(0x651201);
+  static Color primary_30 = Color(0x981b01);
+  static Color primary_40 = Color(0xcb2401);
+  static Color primary_50 = Color(0xfe2d01);
+  static Color primary_60 = Color(0xfe5835);
+  static Color primary_70 = Color(0xfe8167);
+  static Color primary_80 = Color(0xfeab9a);
+  static Color primary_90 = Color(0xffd5cc);
+}
---
+++ lib/src/config/theme/custom_text.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class CustomText {
+  static TextStyle Headline_L = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Headline_M = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Headline_S = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Title_L = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Title_M = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Title_S = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Subtitle_M = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.35,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Heavy_M = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Light_M = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Heavy_S = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Light_S = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Heavy_XS = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Light_XS = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Heavy_XXS = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Body_Light_XXS = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+  static TextStyle Caption_M = TextStyle(
+    fontSize: 109,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.02,
+  );
+}
